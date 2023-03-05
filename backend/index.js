a791405349b8baab31413345232dff714416deb1
HimshikharSingh <himdothat13@gmail.com>
2023-03-05 22:22:07 +0530

    minor changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,10 @@ const mongoDB = require("./db");
 mongoDB();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://master--gentle-muffin-8740bb.netlify.app/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
