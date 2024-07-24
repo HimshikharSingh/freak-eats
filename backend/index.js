d027ec26228baebfd7a517e505746c1df5967e7b
Himshikhar Singh <himdothat13@gmail.com>
2024-07-25 04:18:03 +0530

    new change

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,30 @@
 const express = require("express");
-const cors = require("cors"); // Import the cors package
 const app = express();
 const port = 5000;
 const mongoDB = require("./db");
 mongoDB();
 
-// Set up CORS to allow requests only from the Netlify deployment
-const allowedOrigin = "https://master--freakeats.netlify.app";
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://master--freakeats.netlify.app"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || origin === allowedOrigin) {
-        // Allow requests with no origin (like mobile apps or Postman)
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  })
-);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.use(express.json());
 
 app.use("/api", require("./routes/CreateUser"));
 app.use("/api", require("./routes/DisplayData"));
 app.use("/api", require("./routes/OrderData"));
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
